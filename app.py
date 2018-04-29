e16b05677ce3e851d9dba478ad6446bdb1d93bbb
Travis Taylor <travisdoesmath@gmail.com>
2018-04-29 03:01:54 -0500

    Added functionality to mark seen/unseen in db from website

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 import pymongo
 
 app = Flask(__name__, static_url_path='/static')
@@ -9,9 +9,24 @@ db = client.movies_db
 
 @app.route("/")
 def index():
-    #movies = db.movies.find({'index': { '$gt': 10, '$lt': 20 }})
     movies = db.movies.find()
     return render_template("index.html", movies=movies)
+
+@app.route("/markseen/<title_id>")
+def markseen(title_id):
+    try:
+        db.movies.update({"title_id":title_id}, {'$set':{"seen":True}})
+    except:
+        print(f'An error occured trying to update {title_id}')
+    return redirect("/", code=302)
+
+@app.route("/markunseen/<title_id>")
+def markunseen(title_id):
+    try:
+        db.movies.update({"title_id":title_id}, {'$set':{"seen":False}})
+    except:
+        print(f'An error occured trying to update {title_id}')
+    return redirect("/", code=302)
 
 if __name__ == "__main__":
     app.run(debug=True)
